42dd4acbb523daa06313664aaa72cb0fea00d5aa
Rizvi-07 <tariqul.rizvi@gmail.com>
2019-02-08 22:46:40 +0600

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -2,50 +2,29 @@
 
 int main()
 {
-    int arr1[100];
-	int arr2[100];
-	int arr3[100];
-    int n,mm=1,ctr=0;
-    int i, j;
+    int a[100],b[50],n,i,j,check=1,cnt=0;
 
+    scanf("%d",&n);
 
-       printf("\n\nCount total number of duplicate elements in an array:\n");
-       printf("---------------------------------------------------------\n");
-
-       printf("Input the number of elements to be stored in the array :");
-       scanf("%d",&n);
-
-       printf("Input %d elements in the array :\n",n);
-       for(i=0;i<n;i++)
-        {
-	      printf("element - %d : ",i);
-	      scanf("%d",&arr1[i]);
-	    }
-/*----------------- copy in other array ------------------------------------*/
-		for(i=0;i<n; i++)
-        {
-		arr2[i]=arr1[i];
-		arr3[i]=0;
-        }
-/*------------------- mark the elements are duplicate -------------------------*/
-	for(i=0;i<n; i++)
-        {
-		for(j=0;j<n;j++)
-			{
-				if(arr1[i]==arr2[j])
-				{
-				arr3[j]=mm;
-				mm++;
-				}
-			}
-			mm=1;
-        }
-/*--------------- Prints the array ------------------------------------*/
-   for(i=0; i<n; i++)
-    {
-      if(arr3[i]==2){ctr++;}
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
     }
-      printf("The total number of duplicate elements found in the array is: %d \n", ctr);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++){
+            if(a[i]==a[j]){
+                b[j]=check;
+                check++;
+            }
+        }
+        check=1;
+    }
+    for(i=0;i<n;i++){
+        if(b[i]==2){
+            cnt++;
+        }
+    }
 
-	  printf("\n\n");
+    printf("%d",cnt);
+    return 0;
 }
